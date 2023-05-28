5a663173e69e40830e0ce522e2702e7610f784a5
Lucas Yamamoto <17356081+lucasayb@users.noreply.github.com>
2023-05-28 01:43:54 -0400

    fixed header

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,7 +1,7 @@
 import { Application } from "@hotwired/stimulus"
-
+import Menu from './menu_controller'
 const application = Application.start()
-
+application.register('menu', Menu)
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
